a5a81a79cf707e23c9ec88c54e174e3c3e41e1af
a1shuvo <a1shuvosaha@gmail.com>
2022-03-10 20:27:33 +0600

    Initial Commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Contact Manager App
+
+> This is simple a Contact Manager App using React.
+
+## Technologies Used
+- React
---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = () => {
+    return (   
+        <div className="ui center aligned container">
+            <div className="ui compact menu">
+                <h1>Contact Manager</h1>
+            </div>
+        </div>
+    );
+};
+
+export default Header;
