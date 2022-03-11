2d4e7cfe7dfc31257868bbb01980f7ea0fa19c9a
a1shuvo <a1shuvosaha@gmail.com>
2022-03-11 17:17:59 +0600

    Add and Delete Functionality

--- src/components/ContactList.js
+++ src/components/ContactList.js
@@ -1,9 +1,21 @@
 import React from "react";
+import ContactCard from "./ContactCard";
 
-const ContactList = () => {
+const ContactList = (props) => {
+    // console.log(props);
+
+    const deleteContactHandler = (id) => {
+        props.getContactId(id);
+    }
+
+    const renderContactList = props.contacts.map((contact) => {
+        return (
+            <ContactCard contact = {contact} clickHandler={deleteContactHandler} key={contact.id}/>
+        )
+    });
     return (
-        <div className="ui celled list">
-            Contact List
+        <div className="ui middle aligned celled list">
+            {renderContactList}
         </div>
     )
 }
